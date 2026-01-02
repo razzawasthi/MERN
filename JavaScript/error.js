aab554bcdfc0f78e1509354804593fb882dcb399
Dilli Raj Awasthi <razzawasthi234@gmail.com>
2026-01-02 16:53:17 +0545

    addd

--- JavaScript/error.js
+++ JavaScript/error.js
@@ -1,2 +1,32 @@
 // Error
 
+try {
+    console.log(x);
+}
+catch (error) {
+    // console.log(error);
+    console.log("X xain");
+}
+finally {
+    console.log("finally");
+}
+console.log(200 + 300);
+try{
+    // console.log(x);
+    let x=null
+    console.log(x.toUpperCase());
+}
+catch(error){
+console.log(error.name);
+console.log(error.message);
+// console.log(error.stack);
+
+}
+
+
+
+
+
+
+
+
---
+++ JavaScript/promise.js
@@ -0,0 +1,21 @@
+//promise----->asynchronous data
+//promise create
+// const p=new Promise((resolve,reject)=>{
+//     let isRead=false;
+//     if(isRead){
+//         resolve("Data found")
+//     }
+//     else{
+//         reject("Data not found")
+//     }
+// });
+
+// //promise handle
+// p.then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+//JSON=javascript object notation(stringify form data storage)
+//dumy json ma janney--->for practise
