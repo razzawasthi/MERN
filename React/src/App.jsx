0810ec40530f5496b5299c279e778aeed4121111
Dilli Raj Awasthi <razzawasthi234@gmail.com>
2026-01-09 16:45:27 +0545

    add

---
+++ React/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./App.css"
+
+// const App = () => {
+//   return (
+//     <div >
+//       <div className='box'>
+//         <h1>Hello React </h1>
+//       </div>
+//     </div>
+//   )
+// }
+function App(){
+  return(
+<div className='box'>
+  <h1>Hello React</h1>
+  <h2>Dilli raj Aeasthi</h2>
+</div>
+  )
+}
+export default App
